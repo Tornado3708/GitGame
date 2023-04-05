c7abb8076f291edb4a933ec55233ef64d6aa0fab
Tornado3708 <sami.vagebi@gmail.com>
2023-04-05 15:42:11 +0300

    1

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ class Game {
     console.log(timeStamp)
     this.draw()
     this.move()
-    requestAnimationFrame(this.animate.bind( this ))
+    requestAnimationFrame(() => this.animate.bind( this ))
   }
 
 }
